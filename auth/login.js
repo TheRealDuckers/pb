60c7eaf049396acd4a56cc2d3cb08a652c922bb3
Duckers <198241505+TheRealDuckers@users.noreply.github.com>
2026-02-18 10:28:02 +0000

    fixes

--- auth/login.js
+++ auth/login.js
@@ -1,4 +1,4 @@
-import { auth, db } from "/firebase.js";
+import { auth, db } from "./firebase.js";
 import {
   signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
